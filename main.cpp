4d7383f5cf299f8d7bc927d3db171cfc83b83cfa
Dat Nguyen <datndd260897@gmail.com>
2025-01-18 14:16:03 -0800

    [DatNguyen] update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,18 +13,28 @@ Skeleton code for storage management
 #include <stdexcept>
 #include <cmath>
 #include "classes.h"
+#include "constants.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
     // Initialize the Storage Manager Class with the Binary .dat file name we want to create
-    StorageManager manager("EmployeeRelation.dat");
+    StorageManager manager(DATA_FILE);
 
     // Assuming the Employee.CSV file is in the same directory, 
     // we want to read from the Employee.csv and write into the new data_file
-    manager.createFromFile("Employee.csv");
+    manager.createFromFile(CSV_FILE);
 
     // Searching for Employee IDs Using [manager.findAndPrintEmployee(id)]
-    /***TO_DO***/ 
+    /***TO_DO***/
+    int searchId;
+
+    while (true) {
+        cout << "Enter Employee ID to search (Input 0 to exit): ";
+        cin >> searchId;
+        if (!searchId) break;
+        manager.findAndPrintEmployee(searchId);
+    }
+
 
     return 0;
 }
